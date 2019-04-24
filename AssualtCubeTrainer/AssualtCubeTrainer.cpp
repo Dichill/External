314d255cbeeec772039c6d04c41f50e6f7f051d9
Dichill Tomarong <Dichilltomarong@yahoo.com>
2019-04-24 15:01:48 +0800

    Importing project

---
+++ AssualtCubeTrainer/AssualtCubeTrainer.cpp
@@ -0,0 +1,53 @@
+#include "pch.h"
+#include <iostream>
+#include <vector>
+#include <Windows.h>
+#include "proc.h"
+
+int main()
+{
+	// Design
+	std::cout << "=================================================" << std::endl;
+	std::cout << "         Dichill's Assualt Cube Trainer" << std::endl;
+	std::cout << "=================================================" << std::endl;
+	std::cout << "                                                 " << std::endl;
+	std::cout << "             >> GuidedHacking.com <<" << std::endl;
+
+    // Get ProcID of the target process
+	DWORD procId = GetProcId(L"ac_client.exe");
+
+	// GetModuleBaseAddress
+	uintptr_t moduleBase = GetModuleBaseAddress(procId, L"ac_client.exe");
+
+	// Get Handle To Process
+	HANDLE hProcess = 0;
+	hProcess = OpenProcess(PROCESS_ALL_ACCESS, NULL, procId);
+
+	// Reslove base address fo the pointer Chain
+	uintptr_t dynamicPtrBaseAddr = moduleBase + 0x10F4F4;
+	std::cout << "                                                 " << std::endl;
+	std::cout << "=================================================" << std::endl;
+	std::cout << "          DynamicPtrBaseAddr = " << "0x" << std::hex << dynamicPtrBaseAddr << std::endl;	
+	std::cout << "=================================================" << std::endl;
+	std::cout << "                                                 " << std::endl;
+	// Resolve our ammo pointer chain
+	std::vector<unsigned int> ammoOffsets = {0x374, 0x14, 0x0};
+	uintptr_t ammoAddr = FindDMAAddy(hProcess, dynamicPtrBaseAddr, ammoOffsets);
+
+	// Read Ammo value
+	int ammoValue = 0;
+
+	ReadProcessMemory(hProcess, (BYTE*)ammoAddr, &ammoValue, sizeof(ammoValue), nullptr);
+	std::cout << "Current Ammo = " << std::dec << ammoValue << std::endl;
+
+	// Write to it
+	int newAmmo = 1337;
+	WriteProcessMemory(hProcess, (BYTE*)ammoAddr, &newAmmo, sizeof(newAmmo), nullptr);
+
+	// Read out Again
+	ReadProcessMemory(hProcess, (BYTE*)ammoAddr, &ammoValue, sizeof(ammoValue), nullptr);
+	std::cout << "New Ammo = " << std::dec << ammoValue << std::endl;	
+	getchar();
+	return 0;
+}
+
